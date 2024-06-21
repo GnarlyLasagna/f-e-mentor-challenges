61acab5a83bcc759ab75938a0d7ef93868643ce0
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2024-06-21 12:48:06 -0500

    Add files via upload

---
+++ react-job-listings-with-filtering/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <div class="attribution">
+        <p>
+          Challenge by
+          <a href="https://www.frontendmentor.io">Frontend Mentor</a>. Coded by
+          <a href="https://github.com/gnarlylasagna/" title="Evan">
+            Evan Dolatowski
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
