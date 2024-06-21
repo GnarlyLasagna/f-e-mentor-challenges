449a93d3339498263d4688106d75948e1e123b1a
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2024-06-21 12:49:02 -0500

    Add files via upload

---
+++ todo-app/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <a href="https://github.com/gnarlylasagna/" title="Evan">
+          <div className="attribution" href="https://github.com/gnarlylasagna/" title="Evan">
+          </div>
+        </a>
+      </footer>
+    </>
+  );
+};
